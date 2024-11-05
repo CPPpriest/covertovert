e84e4d15db5f6d72b804989e6ea34d7743282871
priest <mrkilinc22@gmail.com>
2024-11-05 20:03:54 +0300

    icmp sender and receiver sources are implemented and tested successfully.

--- code/receiver/icmp_receiver.py
+++ code/receiver/icmp_receiver.py
@@ -1,3 +1,12 @@
-import scapy
+from scapy.all import send, IP, ICMP, sniff
 
-# Implement your ICMP receiver here
+
+# Function to show captured icmp pckt
+def show_pckt(pckt):
+    # Check packet has ICMP layer && TTL == 1
+    if pckt.haslayer(ICMP) and pckt[IP].ttl == 1:
+        # Display the packet details
+        pckt.show()
+
+# Start sniffing for ICMP packets -> if icmp , then show pckt
+sniff(filter="icmp", prn=show_pckt)
--- code/sender/icmp_sender.py
+++ code/sender/icmp_sender.py
@@ -1,3 +1,13 @@
-import scapy
+from scapy.all import send, IP, ICMP
 
-# Implement your ICMP sender here
+# Destination hostname is "receiver" which is resolved by the DNS
+dest = "receiver"
+
+# Define IP layer with destination and TTL values
+ip_layer = IP(dst=dest, ttl=1)
+
+# Define ICMP layer
+icmp_layer = ICMP()
+
+# Send the created pckt with IP and ICMP layers stacked
+send(ip_layer / icmp_layer)
